82388a5c48180ee51afecc882b09cb55877710a0
Mengistu <mengistunegussie1315@gmail.com>
2022-12-09 06:54:15 -0800

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,8 @@
 #include <stdio.h>
 
 /**
- * main - prints exactly "and that piece of art is useful"
- * - Dora Korpar, 2015-10-19",
- * followed by a new line, to the standard error.
- * Return: Always 0 (Success)
+ * main - Entry point
+ * Return: Always 1 (Success)
  * */
 
 int main(void)
