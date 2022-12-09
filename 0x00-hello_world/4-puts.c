f37c4d2a810b6aef1f7271e496c291552abd21d8
Mengistu <mengistunegussie1315@gmail.com>
2022-12-09 04:34:16 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - prints a given phrase
- * Return: 0 if exited properly, non-zero otherwise
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 
-	puts("\"Programming is like building a multilingual puzzle"): 
+	puts("\"Programming is like building a multilingual puzzle"); 
 		return (0);
 }
