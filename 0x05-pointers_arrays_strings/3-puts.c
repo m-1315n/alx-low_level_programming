0a69951a47af83d2513d91a9e375da24f9dc042f
Mengistu <mengistunegussie1315@gmail.com>
2022-12-20 10:46:04 -0800

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "mai.h"
+
+/**
+ * _puts - write a function that prints a string, following by a new line,
+ * to stdout.
+ *
+ * @str: thi is my input string
+ *
+ */
+
+void _puts(char *str)
+{
+	int index;
+
+	for (index = 0; str[index] != '\0'; index++)
+	{
+		_putchar(str[index]);
+	}
+	_putchar('\n');
+}
