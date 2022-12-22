0128ed89806384edba9ddde04ec23eacaff187db
Mengistu <mengistunegussie1315@gmail.com>
2022-12-22 11:36:22 -0800

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
  * cap_string - function that capitalize the the first character of word
  * @str: string to capitalize
  * Return: returns the capitalized string
  */
-
 char *cap_string(char *str)
 {
 	int index = 0;
